387d6a8eb4d68ab60080162d492be3e949c60ec1
llontores <ezaugenrikh@yandex.kz>
2023-07-02 23:01:50 +0600

    3rd failure

--- Assets/Scripts/EnemyWalking.cs
+++ Assets/Scripts/EnemyWalking.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class EnemyWalking : MonoBehaviour
 {
     [SerializeField] private Transform _path;
     [SerializeField] private float _speed;
+    [SerializeField] private Transform _leftEdge;
+    [SerializeField] private Transform _rightEdge;
 
     private SpriteRenderer _spriteRenderer;
     private Transform[] _points;
     private int _counter;
-    private int _leftEdgeIndex = 1;
-    private int _rightEdgeIndex = 1;
-    private Transform _leftEdge;
-    private Transform _rightEdge;
 
     private void Start()
     {
@@ -24,9 +23,6 @@ public class EnemyWalking : MonoBehaviour
         {
             _points[i] = _path.GetChild(i);
         }
-
-        _leftEdge = _points[_leftEdgeIndex];
-        _rightEdge = _points[_rightEdgeIndex];
     }
 
     private void Update()
@@ -34,19 +30,16 @@ public class EnemyWalking : MonoBehaviour
         Transform target = _points[_counter];
         transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
 
-        if(transform.position == target.position)
+        if (transform.position == target.position)
         {
             _counter++;
 
-            if(_counter >= _points.Length)
+            if (_counter >= _points.Length)
             {
                 _counter = 0;
             }
         }
 
-        if (transform.position == _leftEdge.position)
-            _spriteRenderer.flipX = true;       
-        else
-            _spriteRenderer.flipX= false;
+        _spriteRenderer.flipX = (transform.position.x == _leftEdge.position.x) ? true : (transform.position.x == _rightEdge.position.x) ? false : _spriteRenderer.flipX;
     }
 }
